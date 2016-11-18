de7d62790066bac1699191b4cee303f5a103eaca
andela-lovelyntijesunimi-israel <lovelyn.tijesunimi-israel@andela.com>
2016-11-18 16:40:55 +0100

    [#134574151] Create Quiz COntrollers

--- server/controllers/quiz.controller.js
+++ server/controllers/quiz.controller.js
@@ -5,11 +5,12 @@
   const Quiz = require('./../models/quiz.model');
 
   module.exports ={
-    createNewTask(req, res) {
+    createQuiz(req, res) {
       let userId = req.decoded.id;
       let quiz = new Quiz({
         name: req.body.name,
-        description: req.body.description
+        description: req.body.description,
+        userId: userId
       });
 
       function addQuizIdToUser(id, quiz) {
@@ -20,12 +21,111 @@
         return User
           .findByIdAndUpdate(req.decoded.id, update)
           .exec()
-          .populate('quizzes')
+          .then(() => {
+            res.status(200).json(quiz);
+          })
+          .catch((err) => {
+            res.status(500).json(err);
+          });
       }
       quiz.save()
-        .then((task) => {
-          return
+        .then((quiz) => {
+          return addQuizIdToUser(quiz._id, quiz);
         })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
+
+    getAllQuizzes(req, res) {
+      Quiz.find({}, '-__v')
+        .sort({ 'createdAt': 'descending' })
+        .limit(parseInt(req.query.limit) || parseInt(10))
+        .skip(parseInt(req.query.offset) || parseInt(0))
+        .exec()
+        .then((quizzes) => {
+          if (!quizzes) {
+            return res.status(404).json({
+              message: 'No quizzes found'
+            });
+          }
+          res.status(200).json(quizzes);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
+    getAQuiz(req, res) {
+      Quiz.findById({_id: req.params.id})
+        .exec()
+        .then((quiz) => {
+          if (!quiz) {
+            return res.status(404).json({
+              message: 'No quiz found'
+            });
+          }
+          res.status(200).json(quiz);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
+    getQuizByUser(req, res) {
+      Quiz.find({userId: req.params.userId})
+        .exec()
+        .then((quiz) => {
+          if (!quiz) {
+            return res.status(404).json({
+              message: 'No quiz found'
+            });
+          }
+          res.status(200).json(quiz);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
+    editQuiz(req, res) {
+      let update = {
+        $set: {
+          name: req.body.name,
+          description: req.body.description
+        }
+      };
+      Quiz.findByIdAndUpdate({_id: req.params.id},
+      update, {new:true})
+        .exec()
+        .then((quiz) => {
+          if (!quiz) {
+            return res.status(404).json({
+              message: 'No quiz found'
+            });
+          }
+          res.status(200).json({
+            quiz,
+            message: 'Quiz successfully updated'
+          });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
+    deleteQuiz(req, res) {
+      Quiz.findByIdAndRemove({_id: req.params.id})
+        .exec()
+        .then((quiz) => {
+          if (!quiz) {
+            return res.status(404).json({
+              message: 'No quiz found'
+            });
+          }
+          res.status(200).json({
+            message: 'Quiz successfully deleted'
+          });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
     }
   };
 })();
