c6cd77396adbdb0c05edace5bb31491e8f9fade7
andela-lovelyntijesunimi-israel <lovelyn.tijesunimi-israel@andela.com>
2016-11-18 12:46:09 +0100

    [#134574151] Create Quiz Controllers

---
+++ server/controllers/quiz.controller.js
@@ -0,0 +1,31 @@
+(() => {
+  'use strict';
+
+  const User = require('./../models/user.model');
+  const Quiz = require('./../models/quiz.model');
+
+  module.exports ={
+    createNewTask(req, res) {
+      let userId = req.decoded.id;
+      let quiz = new Quiz({
+        name: req.body.name,
+        description: req.body.description
+      });
+
+      function addQuizIdToUser(id, quiz) {
+        let update = {
+          $push: {quizzes: id}
+        };
+
+        return User
+          .findByIdAndUpdate(req.decoded.id, update)
+          .exec()
+          .
+      }
+      quiz.save()
+        .then((task) => {
+          return
+        })
+    }
+  };
+})();
