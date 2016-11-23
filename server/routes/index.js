a77bf7b00c0738abd84d8d5b9976a3b0a8910e55
andela-lovelyntijesunimi-israel <lovelyn.tijesunimi-israel@andela.com>
2016-11-23 17:53:30 +0100

    [#134574459] Create Question Routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,12 @@
 
   const userRoutes = require('./user.route');
   const quizRoutes = require('./quiz.route');
-  // const questionRoutes = require('./question.route');
+  const questionRoutes = require('./question.route');
 
   module.exports = (router) => {
     userRoutes(router);
     quizRoutes(router);
-    // questionRoutes(router);
+    questionRoutes(router);
   };
 
 })();
